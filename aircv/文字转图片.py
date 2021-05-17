1fd427a999c9aa6b999849a4f29a2b776676e389
liaohui <liaohuio_o@163.com>
2021-05-18 00:00:13 +0800

    test 文字转图片.py

--- aircv/文字转图片.py
+++ aircv/文字转图片.py
@@ -6,7 +6,7 @@ import pygame
 pygame.init()
 
 # 待转换文字
-text = u"  2  "
+text = u"Hello Kitty "
 
 #设置字体和字号
 font = pygame.font.SysFont("arial", 60)
@@ -15,4 +15,6 @@ font = pygame.font.SysFont("arial", 60)
 ftext = font.render(text, True, (0, 0, 0),(255, 255, 255))
 
 #保存图片
-pygame.image.save(ftext, "./resource/text_image.jpg")#图片保存地址
+path = './resource/text_image.jpg'
+temppath = 'hello.jpg'
+pygame.image.save(ftext, temppath)#图片保存地址
