1191439ea44de48f6e1a7c8e8b74450e6a48dbed
liaohui <liaohuio_o@163.com>
2021-05-18 19:25:02 +0800

    white_black

--- line scanf/white_black.py
+++ line scanf/white_black.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
 
+# 反相灰度图，将黑白阈值颠倒
 def accessPiexl(img):
     height = img.shape[0]
     width = img.shape[1]
     for i in range(height):
-       for j in range(width):
-           img[i][j] = 255 - img[i][j]
+        for j in range(width):
+            img[i][j] = 255 - img[i][j]
     return img
+
 
 # 反相二值化图像
 def accessBinary(img, threshold=128):
@@ -18,8 +20,11 @@ def accessBinary(img, threshold=128):
     _, img = cv2.threshold(img, threshold, 0, cv2.THRESH_TOZERO)
     return img
 
+
 path = 'test1.png'
 img = cv2.imread(path, 0)
 img = accessBinary(img)
+
 cv2.imshow('accessBinary', img)
+cv2.imwrite("output.png", img)
 cv2.waitKey(0)
