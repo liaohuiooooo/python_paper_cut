fe1a18768aef2f520360671c1298846688d8f4ea
liaohui <liaohuio_o@163.com>
2021-05-21 19:47:48 +0800

    have a look

--- line scanf/white_black.py
+++ line scanf/white_black.py
@@ -16,13 +16,14 @@ def accessBinary(img, threshold=128):
     img = accessPiexl(img)
     # 边缘膨胀，不加也可以
     kernel = np.ones((3, 3), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    _, img = cv2.threshold(img, threshold, 0, cv2.THRESH_TOZERO)
+    img = cv2.dilate(img, kernel, iterations=2)
+    _, img = cv2.threshold(img, threshold, 0, cv2.THRESH_TOZERO )
     return img
 
 
 path = 'test1.png'
 img = cv2.imread(path, 0)
+
 img = accessBinary(img)
 
 cv2.imshow('accessBinary', img)
